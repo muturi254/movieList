a260b99ce323d2f469f63678dec6c6ccf1b877ed
muturi254 <petermuturi74@gmail.com>
2018-11-12 09:35:10 +0300

    added search view for searching movies reviews class for movie review created search template to show search results & created a search form in index

--- app/request.py
+++ app/request.py
@@ -13,6 +13,29 @@ api_key = app.config['MOVIE_API_KEY']
 base_url = app.config["MOVIE_API_BASE_URL"]
 
 
+def process_results(movie_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    movie_results = []
+    for movie_item in movie_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        movie_object = Movie(id, title, overview, poster,
+                             vote_average, vote_count)
+        movie_results.append(movie_object)
+
+    return movie_results
+
 def get_movies(category):
     '''
     Function that gets the json responce to our url request
@@ -54,26 +77,20 @@ def get_movie(id):
     return movie_object
 
 
-def process_results(movie_list):
-    '''
-    Function  that processes the movie result and transform them to a list of Objects
-    Args:
-        movie_list: A list of dictionaries that contain movie details
-    Returns :
-        movie_results: A list of movie objects
-    '''
-    movie_results = []
-    for movie_item in movie_list:
-        id = movie_item.get('id')
-        title = movie_item.get('original_title')
-        overview = movie_item.get('overview')
-        poster = movie_item.get('poster_path')
-        vote_average = movie_item.get('vote_average')
-        vote_count = movie_item.get('vote_count')
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(
+        api_key, movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
 
-        movie_object = Movie(id, title, overview, poster,
-                             vote_average, vote_count)
-        movie_results.append(movie_object)
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+    return search_movie_results
 
 
-    return movie_results
+
